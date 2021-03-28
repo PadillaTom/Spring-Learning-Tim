f3da4fe1926dd4b4ed856d1a52f9a9a81b76210e
Tomas Padilla <padillatomasagustin@gmail.com>
2021-03-28 08:28:14 +0200

    Comenyamos SPring MVC

--- GuessTheNumberGame/console/src/main/java/academy/learnprogramming/console/ConsoleNumberGuess.java
+++ GuessTheNumberGame/console/src/main/java/academy/learnprogramming/console/ConsoleNumberGuess.java
@@ -4,7 +4,6 @@ import academy.learnprogramming.Game;
 import academy.learnprogramming.MessageGenerator;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.event.ContextRefreshedEvent;
 import org.springframework.context.event.EventListener;
 import org.springframework.stereotype.Component;
@@ -14,40 +13,48 @@ import java.util.Scanner;
 @Component
 public class ConsoleNumberGuess {
 
-//    Constants:
+    // == constants ==
     private static final Logger log = LoggerFactory.getLogger(ConsoleNumberGuess.class);
 
-//    Fields:
-    @Autowired
-    private Game game;
-    @Autowired
-    private MessageGenerator messageGenerator;
+    // == fields ==
+    private final Game game;
 
-//    Events:
+    private final MessageGenerator messageGenerator;
+
+    // == constructors ==
+    public ConsoleNumberGuess(Game game, MessageGenerator messageGenerator) {
+        this.game = game;
+        this.messageGenerator = messageGenerator;
+    }
+
+    // == events ==
     @EventListener(ContextRefreshedEvent.class)
     public void start() {
-        log.info("start() --> Container Ready for use");
+        log.info("start() --> Container ready for use.");
 
-        Scanner scanner = new Scanner (System.in);
-        while(true){
+        Scanner scanner = new Scanner(System.in);
+
+        while(true) {
             System.out.println(messageGenerator.getMainMessage());
             System.out.println(messageGenerator.getResultMessage());
 
-            int guess = scanner.nextInt();
+            int  guess = scanner.nextInt();
             scanner.nextLine();
             game.setGuess(guess);
             game.check();
 
-            if(game.isGameWon()|| game.isGameLost()) {
+            if(game.isGameWon() || game.isGameLost()) {
                 System.out.println(messageGenerator.getResultMessage());
                 System.out.println("Play again y/n?");
 
                 String playAgainString = scanner.nextLine().trim();
-                if(!playAgainString.equalsIgnoreCase("y")){
+                if(!playAgainString.equalsIgnoreCase("y")) {
                     break;
                 }
-            game.reset();
+
+                game.reset();
             }
+
         }
     }
 }
--- GuessTheNumberGame/core/src/main/java/academy/learnprogramming/MessageGeneratorImpl.java
+++ GuessTheNumberGame/core/src/main/java/academy/learnprogramming/MessageGeneratorImpl.java
@@ -2,20 +2,25 @@ package academy.learnprogramming;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
-import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Component;
 
 import javax.annotation.PostConstruct;
 
+@Component
 public class MessageGeneratorImpl implements MessageGenerator {
 
     // == constants ==
     private static final Logger log = LoggerFactory.getLogger(MessageGeneratorImpl.class);
 
     // == fields ==
-    @Autowired
-    private Game game;
+    private final Game game;
 
-    private int guessCount = 10;
+    // == constructor ==
+
+
+    public MessageGeneratorImpl(Game game) {
+        this.game = game;
+    }
 
     // == init ==
     @PostConstruct
@@ -41,7 +46,7 @@ public class MessageGeneratorImpl implements MessageGenerator {
             return "You lost. The number was " + game.getNumber();
         } else if(!game.isValidNumberRange()) {
             return "Invalid number range!";
-        } else if(game.getRemainingGuesses() == guessCount) {
+        } else if(game.getRemainingGuesses() == game.getGuessCount()) {
             return "What is your first guess?";
         } else {
             String direction = "Lower";
@@ -50,7 +55,7 @@ public class MessageGeneratorImpl implements MessageGenerator {
                 direction = "Higher";
             }
 
-            return direction + "! You have " + game.getRemainingGuesses() + " guess left";
+            return direction + "! You have " + game.getRemainingGuesses() + " guesses left";
         }
     }
 }
